49c63da8141917c3371df429e70caaf0ee9a4313
Delfina Troncoso <78504475+delfi23@users.noreply.github.com>
2021-10-27 21:40:49 -0300

    metodo enum toInt() para tests

--- src/turismotierramedia/TipoAtraccion.java
+++ src/turismotierramedia/TipoAtraccion.java
@@ -1,6 +1,22 @@
 package turismotierramedia;
 
 public enum TipoAtraccion {
-	AVENTURA,BELICA, PAISAJE, DEGUSTACION
-
+	AVENTURA,BELICA, PAISAJE, DEGUSTACION;
+	
+	public int toInt() {
+		
+		int valor = 0;
+		
+		if(this == AVENTURA)
+			valor = 1;
+		if(this == BELICA)
+			valor = 2;
+		if(this == DEGUSTACION)
+			valor = 3;
+		if(this == PAISAJE)
+			valor = 4;
+		
+		return valor;
+	}
 }
+
