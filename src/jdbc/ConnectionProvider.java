48de7bde7c642ce65c7e604604676399bbae0b87
Delfina Troncoso <78504475+delfi23@users.noreply.github.com>
2021-10-28 12:45:04 -0300

    Update ConnectionProvider.java

--- src/jdbc/ConnectionProvider.java
+++ src/jdbc/ConnectionProvider.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 
 public class ConnectionProvider {
 	
-	//private static String url1 = "jdbc:sqlite:TierraMedia.db"; //DB sin ID
 	private static String url2 ="jdbc:sqlite:TierraMedia2.db"; //DB con ID
 	private static String url3 ="jdbc:sqlite:TierraMediaTest.db"; //DB para tests
 	private static Connection connection;
